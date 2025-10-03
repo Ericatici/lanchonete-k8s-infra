b06761b91d10c14a9a611b6dc7bb0af8950c5f4c
Ericatici <79939834+Ericatici@users.noreply.github.com>
2025-10-03 17:16:36 -0300

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,1 +1,10 @@
+resource "aws_ecr_repository" "lanchonete_app_ecr" {
+  name                 = "lanchonete-app"
+  image_tag_mutability = "MUTABLE"
+  tags = {
+    Environment = "dev"
+    Project     = "lanchonete"
+  }
+}
+
 
